7f0aa761e88bf002babba32b979a87b4f98fecdf
IlliaSatyrenko <40754663+IlliaSatyrenko@users.noreply.github.com>
2025-12-03 06:19:48 +0200

    feat: add styles for main component

--- frontend/src/app/pages/main/main.ts
+++ frontend/src/app/pages/main/main.ts
@@ -1,11 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit, signal, WritableSignal } from "@angular/core";
 import { Title } from "@shared/components/title/title";
 import { Button } from "@shared/components/button/button";
+import { RouterLink } from "@angular/router";
+import { ArticleCard } from "@components/article/article-card/article-card";
+import { ArticleList } from "@pages/article-list/article-list";
+import { ArticleService } from "@shared/services/article.service";
+import { ArticleType } from "@shared/types/ArticleType";
 
 @Component({
     selector: "app-main",
-    imports: [Title, Button],
+    imports: [Title, Button, RouterLink],
     templateUrl: "./main.html",
     styleUrl: "./main.css",
 })
-export class Main {}
+export class Main implements OnInit {
+    protected readonly recommendedArticles: WritableSignal<ArticleType[]> = signal<ArticleType[]>([]);
+
+    private readonly articleService: ArticleService = inject(ArticleService);
+
+    ngOnInit(): void {
+        this.getRecommendedArticles();
+    }
+
+    protected async getRecommendedArticles(): Promise<void> {
+        const recommended = await this.articleService.getRecommendedArticles(3);
+
+        if (recommended) {
+            this.recommendedArticles.set(recommended);
+        }
+
+        console.log("Recommended articles:", recommended);
+    }
+}
