669e0ca9a3f40586d6f8758f16ea2bfa8ff68b2b
IlliaSatyrenko <denzp05@gmail.com>
2025-12-12 09:12:05 +0200

    fix: refine select and checkbox styles, improve select component

--- frontend/src/app/shared/components/select/select.ts
+++ frontend/src/app/shared/components/select/select.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, input, InputSignal, model, ModelSignal } from "@angular/core";
+import { Component, input, InputSignal, model, ModelSignal, output, OutputEmitterRef } from "@angular/core";
 
 @Component({
     selector: "ui-select",
@@ -11,13 +11,16 @@ export class Select {
     public readonly label: InputSignal<string> = input<string>("");
     public readonly size: InputSignal<"small" | "large"> = input<"small" | "large">("small");
     public readonly theme: InputSignal<"primary"> = input<"primary">("primary");
-    public isDisabled: InputSignal<boolean> = input<boolean>(false);
-    public isFullWidth: InputSignal<boolean> = input<boolean>(false);
+    public readonly isDisabled: InputSignal<boolean> = input<boolean>(false);
+    public readonly isFullWidth: InputSignal<boolean> = input<boolean>(false);
 
-    public value: ModelSignal<string> = model<string>("");
+    public readonly value: ModelSignal<string> = model<string>("");
+    public readonly valueChange: OutputEmitterRef<string> = output<string>();
 
     handleChange(event: Event): void {
         const newValue: string = (event.target as HTMLSelectElement).value;
+        console.log("ui-select change detected:", newValue);
         this.value.set(newValue);
+        this.valueChange.emit(newValue);
     }
 }
